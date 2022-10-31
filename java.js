e69c6070fffe705b83d060a08318026e5a6462e6
mavlyukaev <113362672+mavlyukaev@users.noreply.github.com>
2022-11-01 02:55:42 +0300

    Update java.js

--- java.js
+++ java.js
@@ -1,1 +1,27 @@
+function onClick() {
+    let a = document.getElementById("kol").value;
+    let b = document.getElementById("price").value;
+    let r = document.getElementById("result");
+    a = parseInt(a);
+    b = parseInt(b);
+    r.innerHTML = a * b;
+    return false;
+}
 
+function prov() {
+    let clear = document.getElementById("result");
+    clear.innerHTML = "";
+    let x = document.getElementById("kol").value;
+    let y = document.getElementById("price").value;
+    if ((/\D/.test(x)) || (/\D/.test(y))) {
+        alert("Некорректные данные!");
+    } 
+  else {
+        onClick();
+    }
+}
+
+window.addEventListener("DOMContentLoaded", function () {
+    let c = document.getElementById("my-button");
+    c.addEventListener("click", prov);
+});
