709793ffba4d65cce6c81cdd2f50407bdb1f0c60
mavlyukaev <113362672+mavlyukaev@users.noreply.github.com>
2022-11-01 13:04:58 +0300

    Update java.js

--- java.js
+++ java.js
@@ -1,27 +1,18 @@
-function onClick() {
-    let a = document.getElementById("kol").value;
-    let b = document.getElementById("price").value;
-    let r = document.getElementById("result");
-    a = parseInt(a);
-    b = parseInt(b);
-    r.innerHTML = a * b;
-    return false;
+function onClick(){
+var kolvo=document.getElementById("kolvo").value;
+var stoim=document.getElementById("stoim").value;
+var result=document.getElementById("result");
+var a;
+var b;
+ result.innerHTML = "";
+a = kolvo;
+b = stoim;
+if (a>0 && a!=NaN && b>0 && b!=NaN)
+result.innerHTML=a*b;    
+else result.innerText="Данные введены неверно";    
 }
-
-function prov() {
-    let clear = document.getElementById("result");
-    clear.innerHTML = "";
-    let x = document.getElementById("kol").value;
-    let y = document.getElementById("price").value;
-    if ((/\D/.test(x)) || (/\D/.test(y))) {
-        alert("Некорректные данные!");
-    } 
-  else {
-        onClick();
-    }
-}
-
-window.addEventListener("DOMContentLoaded", function () {
-    let c = document.getElementById("my-button");
-    c.addEventListener("click", prov);
+window.document.addEventListener("DOMContentLoaded", function (event) {
+    console.log("DOM fully loaded and parsed");
+    var b = document.getElementById("btn");
+    b.addEventListener("click", onClick);
 });
