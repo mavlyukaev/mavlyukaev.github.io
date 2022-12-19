0d464431304890340778da0805da14011e2010d0
mavlyukaev <113362672+mavlyukaev@users.noreply.github.com>
2022-12-19 22:42:05 +0300

    Update java7.js

--- java7.js
+++ java7.js
@@ -1,43 +1,25 @@
 var document;
-
 var window;
-
 var alert;
-
 var console;
-
 var $;
 
 $(document).ready(function () {
-
 $(".slider").slick({
-
 arrows: true,
-
 dots: true,
-
 responsive: [
 
 {
-
 breakpoint: 1000,
-
 settings: {
-
 slidesToShow: 2
-
 }
-
 }
-
 ],
 
 slidesToScroll: 4,
-
 slidesToShow: 4,
-
 waitForAnimate: false
-
 });
-
 });
